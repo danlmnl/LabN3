13ebaed7f415227647c5274532d2a5cf5148d68f
danlmnl <dmanujlo@gmail.com>
2025-12-24 04:19:59 +0300

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,47 +1,58 @@
 public class Test {
     public static void main(String[] args) {
-        //        ТЕСТ 1 - создание списка кандидатов и вывод списка
+
+        System.out.println("ТЕСТ 1 - создание списка кандидатов и вывод списка");
         Election election = new Election(5);
         election.coutCandidates();
-        //        ТЕСТ 2 - добавление нового кандидата
+
+        System.out.println("\nТЕСТ 2 - добавление нового кандидата");
         election.addCandidate("Петров");
-        //        ТЕСТ 3 - добавление нового кандидата с заданным числом голосов
+
+        System.out.println("\nТЕСТ 3 - добавление нового кандидата с заданным числом голосов");
         election.addCandidate("Иванов", 20);
         election.addCandidate("Сидоров", 10);
-        election.addCandidate("Сидоров", 10); // добавление одного и того же кандидата вызывает текстовое сообщение
-        //        ТЕСТ 4 - поиск кандидата по имени (индекс)
+        election.addCandidate("Сидоров", 10); // повторный кандидат
+
+        System.out.println("\nТЕСТ 4 - поиск кандидата по имени (индекс)");
         System.out.println(election.findIndex("Иванов"));
-        //        ТЕСТ 5 - удаление кандидата
+
+        System.out.println("\nТЕСТ 5 - удаление кандидата");
         election.removeCandidate("Петров");
         election.coutCandidates();
-        //        ТЕСТ 6 - добавление голоса кандидату
+
+        System.out.println("\nТЕСТ 6 - добавление голоса кандидату");
         election.addVoteToCandidate("Сидоров");
         election.coutCandidates();
-        //        ТЕСТ 7 - добавление n голосов кандидату
+
+        System.out.println("\nТЕСТ 7 - добавление n голосов кандидату");
         election.addVotesToCandidate("Иванов", 3);
         election.coutCandidates();
-        //        ТЕСТ 8 - получить количество голосов за кандидата
+
+        System.out.println("\nТЕСТ 8 - получение количества голосов кандидата");
         System.out.println(election.getCandidatesVotes("Иванов"));
-        //        ТЕСТ 9 - получить общее количество голосов
+
+        System.out.println("\nТЕСТ 9 - получение общего количества голосов");
         System.out.println(election.getTotalVotes());
-        //        ТЕСТ 10 - определение победителя голосования
+
+        System.out.println("\nТЕСТ 10 - определение победителя голосования");
         System.out.println(election.getWinnerOfElection());
-        //        ТЕСТ 11 - получить процент проголосовавших за кандидата
+
+        System.out.println("\nТЕСТ 11 - получение процента голосов кандидата");
         System.out.println(election.getResCandidate("Сидоров"));
-        //        ТЕСТ 12 - вывод таблицы с процентами проголосовавших за кандидатов
+
+        System.out.println("\nТЕСТ 12 - вывод таблицы процентного распределения голосов");
         election.coutPercents();
-        //        ТЕСТ 13 - удаление кандидатов с малым количеством голосов
-        election.removeLosers(5);
+
+        System.out.println("\nТЕСТ 13 - удаление кандидатов с малым количеством голосов");
+        election.removeLosers(12);
         election.coutCandidates();
-        //        ТЕСТ 14 - подсчет голосов из массива имён
+
+        System.out.println("\nТЕСТ 14 - подсчёт голосов из массива имён");
         String[] k = new String[3];
-        k[0] = new String("Ибрагимов");
-        k[1] = new String("Осипов");
-        k[2] = new String("Шпехт");
+        k[0] = "Фетисов";
+        k[1] = "Конкина";
+        k[2] = "Шпехт";
         election.bullitens(k);
         election.coutCandidates();
-
-
     }
 }
-
